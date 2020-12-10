7e198a1fa4ae624c52937a7d8a0f88758b387cae
enerdje <enerdje@inbox.ru>
2020-12-10 21:17:12 +0600

    Рефакторинг тестов.

--- AdventOfCode.Tests/Helper.cs
+++ AdventOfCode.Tests/Helper.cs
@@ -13,24 +13,3 @@ namespace AdventOfCode.Tests
 			new List<int>(File.ReadLines(path).Select(int.Parse).ToList());
 	}
 }
-
-//public void xd()
-//{
-//	var sequence = Enumerable.Range(1, 100).ToList();
-//	var filter = sequence.Where(o => o > 50)
-//		.Select(s => string.Concat("*", s.ToString(), "*"));
-//	var firstChet = sequence.First(o => o % 2 == 0);
-
-//	//var agr = sequence.Aggregate();
-
-//	foreach (var item in filter)
-//	{
-//		Console.WriteLine(item);
-//	}
-//	Console.WriteLine("Первое чётное: {0}", firstChet);
-//}
-
-//public static string GetStringValue()
-//{
-//	return "Vladimir";
-//}
--- AdventOfCode.Tests/Tests.cs
+++ AdventOfCode.Tests/Tests.cs
@@ -3,8 +3,10 @@ using System.Linq;
 
 namespace AdventOfCode.Tests
 {
+	public class Tests { }
+
 	[TestFixture]
-	public class Day1
+	public class Day1_Tests
 	{
 		[SetUp]
 		public void Setup()
@@ -29,13 +31,35 @@ namespace AdventOfCode.Tests
 		public void Quest_2()
 		{
 			var firstDay = new Days.Day1();
-			var list = Helper.GetListIntValues(Helper.GetPathValues("day1.txt"));
+			var listValues = Helper.GetListIntValues(Helper.GetPathValues("day1.txt"));
 
-			var result = firstDay.Quest_2(list);
+			var result = firstDay.Quest_2(listValues);
 			var multiplicationResult = result.Aggregate((p, x) => p *= x);
 
 			Assert.True(result.Sum() == 2020);
 			Assert.True(multiplicationResult == 173538720);
+		}
+	}
+
+	[TestFixture]
+	public class Day2_Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+
+		}
+
+		[Test]
+		public void Quest_1()
+		{
+
+		}
+
+		[Test]
+		public void Quest_2()
+		{
+
 		}
 	}
 }
